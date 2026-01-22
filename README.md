9d6efc165d258c9a82072fa9cc5e1273c2e452df
Guilherme Teixeira <gteixeira.dev@outlook.com>
2026-01-22 14:44:30 -0300

    feat: receber nome de usuário e encontrá-lo na API que foi linkada via base_URL e estilo aplicado ao buscar e a promise for concluida.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # git-profile-view
 Aplicação web que permite buscar usuários do GitHub e visualizar informações de perfil e repositórios. Desenvolvida com foco em simplicidade inicial, evolução incremental e boas práticas de arquitetura front-end com JavaScript moderno.
+
+Endpoints que vamos utilizar:
+
+Perfil: GET/users/:username
+Repositórios: GET/users/:username/repos
---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+const inputSearch = document.getElementById('input-search');
+const btnSearch = document.getElementById('btn-search');
+const profileResults = document.querySelector(".profile-results")
+
+const base_URL = "https://api.github.com"
+
+btnSearch.addEventListener("click", async () => {
+    const userName = inputSearch.value;
+    if (userName) {
+        try {
+            const response = await fetch(`${base_URL}/users/${userName}`);
+
+            if (!response) {
+                alert("Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente.");
+                return;
+            }
+
+            const userData = await response.json();
+            console.log(userData);
+
+            profileResults.innerHTML = `
+            <div class="profile-card">
+                <img src="${userData.avatar_url}" alt="Foto de ${userData.name}" class="profile-avatar">
+                <div class="profile-info">
+                    <h2>${userData.name}</h2>
+                    <p>${userData.bio || "Não possui bio cadastrada 😢"}</p>
+                </div>
+            </div>
+            `
+        } catch (error) {
+            console.error("Erro ao buscar perfil do usuário:", error);
+            alert("Ocorreu um erro ao buscar o perfil do usuário. Por favor, tente novamente mais tarde.");
+            
+        }
+    } else {
+        alert("Por favor, digite um nome de usuário do GitHub!");
+    }
+
+});
