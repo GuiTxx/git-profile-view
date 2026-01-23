9d3f6a3973bd88bec71d9a5324d51ee4dfc277e6
Guilherme Teixeira <gteixeira.dev@outlook.com>
2026-01-23 14:24:49 -0300

    feat: criação  do mostrativo de seguidores e seguindo.

--- src/js/index.js
+++ src/js/index.js
@@ -29,6 +29,17 @@ btnSearch.addEventListener("click", async () => {
                     <p>${userData.bio || "Não possui bio cadastrada 😢"}</p>
                 </div>
             </div>
+
+            <div class="profile-counters">
+                <div class="followers">
+                    <h4>👥Seguidores</h4>
+                    <span>${userData.followers}</span>
+                </div>
+                <div class="following">
+                    <h4>👥Seguindo</h4>
+                    <span>${userData.following}</span>
+                </div>
+            </div>
             `
         } catch (error) {
             console.error("Erro ao buscar perfil do usuário:", error);
